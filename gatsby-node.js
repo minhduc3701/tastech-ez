bd1a59605b9ddeefc5088e3092774ffd15dd72ec
thanh4890 <thanh4890@gmail.com>
2020-03-03 09:50:46 +0700

    Update credit for onCreatePage

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,7 @@ const {langs} = require('./src/data/config')
 const isIndexPage = (page, lang) => page.path === `/${lang}`
 const is404Page = page => page.path.startsWith('/404')
 
+// @see https://youtu.be/wSIw0mu3Q1U
 exports.onCreatePage = ({page, actions}) => {
   const skip = langs.some(lang => isIndexPage(page, lang)) || is404Page(page)
 
