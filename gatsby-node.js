82e49b1034b4fa6be9cbdfecbc4406e883befc03
phan2796 <phan2796@gmail.com>
2020-04-02 12:11:45 +0700

    redirect /id and /id/ to homepage

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,6 +19,19 @@ exports.onCreatePage = ({page, actions}) => {
 exports.createPages = ({ graphql, actions }) => {
   const { createRedirect } = actions;
 
+  createRedirect({
+    fromPath: `/id`,
+    toPath: `/`,
+    isPermanent: true,
+    redirectInBrowser: true,
+  });
+  createRedirect({
+    fromPath: `/id/`,
+    toPath: `/`,
+    isPermanent: true,
+    redirectInBrowser: true,
+  });
+
   // Oops
   createRedirect({
     fromPath: `/feature`,
