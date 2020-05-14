2690f197a66bbd32e18c396f56b24b950666cf63
phan2796 <phan2796@gmail.com>
2020-05-14 18:22:23 +0700

    close button

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Header from './Header'
 import Footer from './Footer'
@@ -9,7 +9,6 @@ import 'intl';
 import styled from 'styled-components'
 import { Main } from '../styles'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'gatsby'
 import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { css } from 'glamor'
@@ -42,56 +41,74 @@ const Wrapper = styled(Main)`
 `
 
 const Content = styled.main`
-  margin-top: ${props => props.campaign ? '104px': '64px' }; 
+  margin-top: ${props => props.campaign ? '104px' : '64px'}; 
 
   @media screen and (max-width: 1199px) {
-     margin-top: ${props => props.campaign ? '96px': '56px' }; 
+     margin-top: ${props => props.campaign ? '96px' : '56px'}; 
   }
   @media screen and (max-width: 767px) {
-    margin-top: ${props => props.campaign ? '110px': '56px' }; 
+    margin-top: ${props => props.campaign ? '110px' : '56px'}; 
  }
 `
 
 
-const Layout = ({ children, location }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query LayoutQuery {
-          site {
-            siteMetadata {
-              languages {
-                defaultLangKey
-                langs
-              }
+class Layout extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showCampaign: true,
+      children: this.props.children,
+      location: this.props.location,
+    }
+  }
+
+  hideCampaign = () => {
+    this.setState({
+      showCampaign: false
+    })
+  }
+
+  render() {
+    let { showCampaign, children, location } = this.state
+    return (
+      <StaticQuery
+        query={graphql`
+      query LayoutQuery {
+        site {
+          siteMetadata {
+            languages {
+              defaultLangKey
+              langs
             }
           }
         }
-      `}
-      render={data => {
-        const url = location.pathname;
-        const { langs, defaultLangKey } = data.site.siteMetadata.languages;
-        const langKey = getCurrentLangKey(langs, defaultLangKey, url);
-        const homeLink = `/${langKey}/`.replace(`/${defaultLangKey}/`, '/');
-        const langsMenu = getLangs(langs, langKey, getUrlForLang(homeLink, url)).map((item) => ({ ...item, link: item.link.replace(`/${defaultLangKey}/`, '/') }));
-        return (
-          <Wrapper>
-           
-            <IntlProvider
-              locale={langKey}
-              messages={messages[langKey]}
-            >
-              <Header langs={langsMenu} langKey={langKey} langUri={homeLink} />
-              <Content campaign={true}>
-                {children({ langKey, defaultLangKey, homeLink })}
-              </Content>
-              <Footer langKey={langKey} langUri={homeLink} />
-            </IntlProvider>
-          </Wrapper>
-        )
-      }}
-    />
-  );
+      }
+    `}
+        render={data => {
+          const url = location.pathname;
+          const { langs, defaultLangKey } = data.site.siteMetadata.languages;
+          const langKey = getCurrentLangKey(langs, defaultLangKey, url);
+          const homeLink = `/${langKey}/`.replace(`/${defaultLangKey}/`, '/');
+          const langsMenu = getLangs(langs, langKey, getUrlForLang(homeLink, url)).map((item) => ({ ...item, link: item.link.replace(`/${defaultLangKey}/`, '/') }));
+          return (
+            <Wrapper>
+
+              <IntlProvider
+                locale={langKey}
+                messages={messages[langKey]}
+              >
+                <Header langs={langsMenu} langKey={langKey} langUri={homeLink} hideCampaign={this.hideCampaign} showCampaign={showCampaign} />
+                <Content campaign={showCampaign}>
+                  {children({ langKey, defaultLangKey, homeLink })}
+                </Content>
+                <Footer langKey={langKey} langUri={homeLink} />
+              </IntlProvider>
+            </Wrapper>
+          )
+        }}
+      />
+    )
+  }
 };
 
 export const layoutWithLangKey = C => props => (
